1d448986cb4ad1e94ed64d3b4116dbb573e3b4ab
S2ealea <s2ealea@example.com>
2024-01-25 23:49:01 +0800

    Add Exercise:print a welcome text in separate line.

---
+++ CPP_nano_Projects/CPP_Exercises/CS107_C++_Programming/Unit1_Introduction_and_Setup/Exec_01_welcome_text.cpp
@@ -0,0 +1,9 @@
+// https://learn.saylor.org/mod/book/view.php?id=33554
+// Write a program in C++ to print a welcome text in a separate line.
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to the C++'s world!" << std::endl;
+    return 0;
+}
