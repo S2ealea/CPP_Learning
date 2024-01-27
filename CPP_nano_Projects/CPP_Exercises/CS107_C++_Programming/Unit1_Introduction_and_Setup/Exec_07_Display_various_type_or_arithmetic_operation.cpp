e5cf8dfdca2393142beb18d4632ea8738de4e578
S2ealea <s2ealea@example.com>
2024-01-27 10:12:21 +0800

    Add Exercise:Display various type or arithmetic operation using mixed data type.

---
+++ CPP_nano_Projects/CPP_Exercises/CS107_C++_Programming/Unit1_Introduction_and_Setup/Exec_07_Display_various_type_or_arithmetic_operation.cpp
@@ -0,0 +1,40 @@
+// Write a program in C++ to display various type or arithmetic operation using mixed data type.
+/*
+Sample output:
+
+Display arithmetic operations with mixed data type :
+---------------------------------------------------------
+5 + 7 = 12
+3.7 + 8.0 = 11.7
+5 + 8.0 = 13.0
+5 - 7 = -2
+3.7 - 8.0 = -4.3
+5 - 8.0 = -3.0
+5 * 7 = 35
+3.7 * 8.0 = 29.6
+5 * 8.0 = 40.0
+5 / 7 = 0
+3.7 / 8.0 = 0.5
+5 / 8.0 = 0.6
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Display arithmetic operations with mixed data type :" << std::endl;
+    std::cout << "5 + 7 = " << 5+7 << std::endl;
+    std::cout << "3.70 + 8.0 = " << 3.70 + 8.0 << std::endl;
+    std::cout << "5 + 8.0 = " << 5 + 8.0 << std::endl;
+    std::cout << "5 -7 = " << 5 -7 << std::endl;
+    std::cout << "3.7 - 8.0 = " << 3.7 - 8.0 << std::endl;
+    std::cout << "5 - 8.0 = " << 5 -8.0 << std::endl;
+    std::cout << "5 * 7 = " << 5 * 7 << std::endl;
+    std::cout << "3.7 * 8.0 = " << 3.7 * 8.0 << std::endl;
+    std::cout << "5 * 8.0 = " << 5 * 8.0 << std::endl;
+    std::cout << "5 / 7 = " << 5 / 7 << std::endl;
+    std::cout << "3.7 / 8.0 = " << 3.7 / 8.0 << std::endl;
+    std::cout << " 5 / 8.0 = " << 5 /8.0 << std::endl;
+
+    return 0;
+}
