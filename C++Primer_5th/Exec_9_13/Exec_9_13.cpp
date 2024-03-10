4052ff8b193bbb5f8df898de5b97624f40338496
S2ealea <s2ealea@example.com>
2024-03-10 17:20:15 +0800

    Add Exec_9_13:Initialize vector<double> by list<int> or vector<int>.

---
+++ C++Primer_5th/Exec_9_13/Exec_9_13.cpp
@@ -0,0 +1,7 @@
+# Initialize a vector<double> from list<int> or vector<int>.
+
+list<int> ilist(5,4);
+vector<int> ivec(5,5);
+
+vector<double> dvc(ilist.begin(), ilist.end());
+vector<double> dev2(ivec.begin(), ivec.end());
