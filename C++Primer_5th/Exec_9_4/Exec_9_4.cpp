ef6d69c977cbf3237e8fa604a4a5a4cc9e8dd625
S2ealea <s2ealea@example.com>
2024-03-10 17:07:23 +0800

    Add Exec_9_4:A function with the vector<int> as parameter.

---
+++ C++Primer_5th/Exec_9_4/Exec_9_4.cpp
@@ -0,0 +1,10 @@
+bool find(vector<int>::const_iterator begin, vector<int>::const_iterator end, int i)
+{
+    for ()
+    while ( begin++ != end)
+    {
+        if (*begin == i)
+            return ture;
+    }
+    return false;
+}
